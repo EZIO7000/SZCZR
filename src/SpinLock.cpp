88590f0da7b848bd29871db22f8e3c6be7389e27
Jakub Rybalko <rybalkojakubpiotr@gmail.com>
2021-01-18 17:27:20 +0100

    add loop and fix (check if it works)

--- src/SpinLock.cpp
+++ src/SpinLock.cpp
@@ -30,76 +30,75 @@ void initSharedMemory()
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-
 }
 
-void processA(pthread_spinlock_t lock, int ret)
+void processA(pthread_spinlock_t lockA, pthread_spinlock_t lockB, int ret)
 {
-
-    ret = pthread_spin_lock(&lock);
 
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-    
+
     int rate = 44100;
     const uint16_t freq = 250;
-    long unsigned int bufferSize = 4087*4;
-    const uint16_t len = bufferSize*16;
+    long unsigned int bufferSize = 4087 * 4;
+    const uint16_t len = bufferSize * 16;
     const float_t arg = 2 * 3.141592 * freq / rate;
-    long int vals[len+1];
+    long int vals[len + 1];
     int i = 0;
-    for(i; i < len; i = i + 1) {
-        vals[i] = SHRT_MAX * sin(arg*i);
+    for (i; i < len; i = i + 1)
+    {
+        vals[i] = SHRT_MAX * sin(arg * i);
     }
 
-    
+    // loop from here I guess idk
+    int a = 0;
+    while (a < 30)
+    {
+        std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
+        auto duration = startTime.time_since_epoch();
+        auto nano = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+        vals[len] = nano;
+        std::cout << "A" << std::endl;
 
+        if (pthread_spin_trylock(&lockA))
+        {
+            printf("Proc A: wait for the spinlock...\n");
+            pthread_spin_lock(&lockA);
+        }
+        memcpy(str, vals, sizeof(vals));
+        ret = pthread_spin_unlock(&lockB);
+        std::cout << "A" << std::endl;
+        a++;
+    }
 
-// loop from here I guess idk
-
-    std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
-    auto duration = startTime.time_since_epoch();
-    auto nano = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
-    vals[len] = nano;
-    std::cout<<"A"<<std::endl;
-    memcpy(str, vals, sizeof(vals));
-    std::cout<<"A"<<std::endl;
-
-
-    ret = pthread_spin_unlock(&lock);
     shmdt(str); // <- to poza petla powinno byc nie? po unlock'u
-
 }
 
-void processB(pthread_spinlock_t lock, int ret1)
+void processB(pthread_spinlock_t lockA, pthread_spinlock_t lockB, int ret1)
 {
-    ret1 = pthread_spin_lock(&lock);
-
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-    
-    
+
     int ret;
 
-    snd_pcm_t* pcm_handle;  // device handle
+    snd_pcm_t *pcm_handle; // device handle
     snd_pcm_stream_t stream = SND_PCM_STREAM_PLAYBACK;
-    snd_pcm_hw_params_t* hwparams;  // hardware information
-    char* pcm_name = strdup("plughw:0,0");  // on-board audio jack
+    snd_pcm_hw_params_t *hwparams;         // hardware information
+    char *pcm_name = strdup("plughw:0,0"); // on-board audio jack
     int rate = 44100;
 
     const uint16_t freq = 240;
-    long unsigned int bufferSize = 4087*4;
-    const uint16_t len = bufferSize*16;
+    long unsigned int bufferSize = 4087 * 4;
+    const uint16_t len = bufferSize * 16;
     const float_t arg = 2 * 3.141592 * freq / rate;
     uint16_t vals[len];
     int i = 0;
-    for(i; i < len; i = i + 1) {
-        vals[i] = 10 * sin(arg*i);
+    for (i; i < len; i = i + 1)
+    {
+        vals[i] = 10 * sin(arg * i);
     }
-
-    
 
     snd_pcm_hw_params_alloca(&hwparams);
 
@@ -107,71 +106,80 @@ void processB(pthread_spinlock_t lock, int ret1)
     //std::cout << "Opening: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_any(pcm_handle, hwparams);
-    //std::cout << "Initializing hwparams structure: " << snd_strerror(ret) << std::endl;   
+    //std::cout << "Initializing hwparams structure: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_access(pcm_handle, hwparams,
-            SND_PCM_ACCESS_RW_INTERLEAVED);
+                                       SND_PCM_ACCESS_RW_INTERLEAVED);
     //std::cout << "Setting access: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_format(pcm_handle, hwparams,
-            SND_PCM_FORMAT_S16_LE);
+                                       SND_PCM_FORMAT_S16_LE);
     //std::cout << "Setting format: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_rate(pcm_handle, hwparams,
-            rate, (int)0);
+                                     rate, (int)0);
     //std::cout << "Setting rate: " << snd_strerror(ret) << std::endl;
 
-    ret = snd_pcm_hw_params_set_channels(pcm_handle, hwparams, 2); 
+    ret = snd_pcm_hw_params_set_channels(pcm_handle, hwparams, 2);
     //std::cout << "Setting channels: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_periods(pcm_handle, hwparams, 2, 0);
     //std::cout << "Setting periods: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_buffer_size_near(pcm_handle, hwparams,
-            &bufferSize);
+                                                 &bufferSize);
     //std::cout << "Setting buffer size: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params(pcm_handle, hwparams);
     //std::cout << "Applying parameters: " << snd_strerror(ret) << std::endl;
 
- // loop from here
-    
-    std::cout<<"B"<<std::endl;
-    long int valsTmp[len+1];
+    // loop from here
 
-    memcpy(&valsTmp, str, sizeof(valsTmp));
-    std::cout<<"B"<<std::endl;
-    
-    clock_t startTime = valsTmp[len];
+    int loop = 0;
+    while (loop < 30)
+    {
+        if (pthread_spin_trylock(&lockB))
+        {
+            printf("Proc B: wait for the spinlock...\n");
+            pthread_spin_lock(&lockB);
+        }
+        //ret1 = pthread_spin_lock(&lock);
+
+        std::cout << "B" << std::endl;
+        long int valsTmp[len + 1];
+
+        memcpy(&valsTmp, str, sizeof(valsTmp));
+        std::cout << "B" << std::endl;
+
+        clock_t startTime = valsTmp[len];
 
         i = 0;
-        for(i; i < len; i = i + 1) {
+        for (i; i < len; i = i + 1)
+        {
             vals[i] = valsTmp[i];
         }
 
-    std::chrono::time_point<std::chrono::system_clock> endTimeTmp = std::chrono::system_clock::now();
-    auto duration = endTimeTmp.time_since_epoch();
-    auto endTime = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+        std::chrono::time_point<std::chrono::system_clock> endTimeTmp = std::chrono::system_clock::now();
+        auto duration = endTimeTmp.time_since_epoch();
+        auto endTime = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
 
-
-    int err;
-    const void* ptra = (const void*)&vals;
-    err = snd_pcm_prepare(pcm_handle);
-    //std::cout << "Preparing: " << snd_strerror(err)<< std::endl;
-    while(err!=0)
-        {   
-            err = snd_pcm_prepare(pcm_handle);  
+        int err;
+        const void *ptra = (const void *)&vals;
+        err = snd_pcm_prepare(pcm_handle);
+        //std::cout << "Preparing: " << snd_strerror(err)<< std::endl;
+        while (err != 0)
+        {
+            err = snd_pcm_prepare(pcm_handle);
         }
-    snd_pcm_writei(pcm_handle, ptra, len/2);
-    std::printf("loop nr  i ;%ld micorseconds; \n",/*a,*/(endTime - startTime));
+        snd_pcm_writei(pcm_handle, ptra, len / 2);
+        std::printf("loop nr  i ;%ld micorseconds; \n", /*a,*/ (endTime - startTime));
 
-//endloop
-    ret1 = pthread_spin_unlock(&lock);
-    
+        //endloop
+        ret1 = pthread_spin_unlock(&lockA);
+    }
+
     shmdt(str);
     shmctl(shmid, IPC_RMID, NULL);
-
-    
 }
 
 void createProc(void (*function)())
@@ -185,46 +193,49 @@ void createProc(void (*function)())
 
 int main()
 {
-    // INSTRUKCJA DO ODPALENIA 
+    // INSTRUKCJA DO ODPALENIA
     // g++ src/SpinLock.cpp -pthread -o SpinLock  -lstdc++ -pthread -lrt -lasound
     // ./SpinLock
 
-    pthread_spinlock_t lock;
+    pthread_spinlock_t lockA;
+    pthread_spinlock_t lockB;
     int pshared;
     int ret;
-    ret = pthread_spin_init(&lock, pshared);
+    int retB;
+    ret = pthread_spin_init(&lockA, pshared);
+    retB = pthread_spin_init(&lockB, pshared);
 
     initSharedMemory();
 
-    std::cout<<"main"<<std::endl;
+    std::cout << "main" << std::endl;
     //createProc(processA);
 
     //creating new process
     if (fork() == 0)
     {
-        processA(lock, ret);
+        processA(lockA, lockB, ret);
         exit(0);
     }
 
-    std::cout<<"main"<<std::endl;
+    std::cout << "main" << std::endl;
     sleep(3);
     //createProc(processB);
 
     //creating new process
     if (fork() == 0)
     {
-        processB(lock, ret);
+        processB(lockA, lockB, ret);
         exit(0);
     }
 
-    std::cout<<"main"<<std::endl;
+    std::cout << "main" << std::endl;
 
     while (wait(NULL) > 0)
     {
-        
     }
-    
-    ret = pthread_spin_destroy(&lock);
-    
+
+    ret = pthread_spin_destroy(&lockA);
+    retB = pthread_spin_destroy(&lockB);
+
     return 0;
 }
--- src/mutex.cpp
+++ src/mutex.cpp
@@ -32,165 +32,171 @@ void initSharedMemory()
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-
 }
 
-void processA()
+void processA(shared_mutex_t mutexA, shared_mutex_t mutexB)
 {
-    shared_mutex_t mutex = shared_mutex_init("/my-mutex");
-    if (mutex.ptr == NULL)
-    {
-        std::cout << "Could not initialise mutex" << std::endl;
-        return;
-    }
-    pthread_mutex_lock(mutex.ptr);
+    // shared_mutex_t mutex = shared_mutex_init("/my-mutex");
+    // if (mutex.ptr == NULL)
+    // {
+    //     std::cout << "Could not initialise mutex" << std::endl;
+    //     return;
+    // }
 
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-    
 
-int rate = 44100;
-const uint16_t freq = 250;
-long unsigned int bufferSize = 4087*4;
-const uint16_t len = bufferSize*16;
-const float_t arg = 2 * 3.141592 * freq / rate;
-long int vals[len+1];
-int i = 0;
-for(i; i < len; i = i + 1) {
-    vals[i] = SHRT_MAX * sin(arg*i);
-}
+    int rate = 44100;
+    const uint16_t freq = 250;
+    long unsigned int bufferSize = 4087 * 4;
+    const uint16_t len = bufferSize * 16;
+    const float_t arg = 2 * 3.141592 * freq / rate;
+    long int vals[len + 1];
+    int i = 0;
+    for (i; i < len; i = i + 1)
+    {
+        vals[i] = SHRT_MAX * sin(arg * i);
+    }
 
-//petla od tąd
-    std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
-    auto duration = startTime.time_since_epoch();
-    auto nano = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
-    vals[len] = nano;
-   // pthread_mutex_lock(mutex.ptr);
-    std::cout<<"A"<<std::endl;
-    memcpy(str, vals, sizeof(vals));
-    std::cout<<"A"<<std::endl;
-   // pthread_mutex_unlock(mutex.ptr);
-    
+    //petla od tąd
+    int loop = 0;
+    while (loop < 30)
+    {
+        pthread_mutex_lock(mutexA.ptr);
+        std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
+        auto duration = startTime.time_since_epoch();
+        auto nano = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+        vals[len] = nano;
+        // pthread_mutex_lock(mutex.ptr);
+        std::cout << "A" << std::endl;
+        memcpy(str, vals, sizeof(vals));
+        std::cout << "A" << std::endl;
+        // pthread_mutex_unlock(mutex.ptr);
 
+        loop++;
+        pthread_mutex_unlock(mutexB.ptr);
+    }
     //sleep(20);
     shmdt(str);
 
-    pthread_mutex_unlock(mutex.ptr);
-    shared_mutex_close(mutex);
+    //pthread_mutex_unlock(mutex.ptr);
+    //shared_mutex_close(mutex);
 }
 
-void processB()
+void processB(shared_mutex_t mutexA, shared_mutex_t mutexB)
 {
-    shared_mutex_t mutex = shared_mutex_init("/my-mutex");
-    if (mutex.ptr == NULL)
-    {
-        std::cout << "Could not initialise mutex" << std::endl;
-        return;
-    }
-    pthread_mutex_lock(mutex.ptr);
+    // shared_mutex_t mutex = shared_mutex_init("/my-mutex");
+    // if (mutex.ptr == NULL)
+    // {
+    //     std::cout << "Could not initialise mutex" << std::endl;
+    //     return;
+    // }
 
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 32000000, 0666 | IPC_CREAT);
     unsigned char *str = (unsigned char *)shmat(shmid, (void *)0, 0);
-    
 
     int ret;
 
-    snd_pcm_t* pcm_handle;  // device handle
+    snd_pcm_t *pcm_handle; // device handle
     snd_pcm_stream_t stream = SND_PCM_STREAM_PLAYBACK;
-    snd_pcm_hw_params_t* hwparams;  // hardware information
-    char* pcm_name = strdup("plughw:0,0");  // on-board audio jack
+    snd_pcm_hw_params_t *hwparams;         // hardware information
+    char *pcm_name = strdup("plughw:0,0"); // on-board audio jack
     int rate = 44100;
 
     const uint16_t freq = 240;
-    long unsigned int bufferSize = 4087*4;
-    const uint16_t len = bufferSize*16;
+    long unsigned int bufferSize = 4087 * 4;
+    const uint16_t len = bufferSize * 16;
     const float_t arg = 2 * 3.141592 * freq / rate;
     uint16_t vals[len];
     int i = 0;
-    for(i; i < len; i = i + 1) {
-        vals[i] = 10 * sin(arg*i);
+    for (i; i < len; i = i + 1)
+    {
+        vals[i] = 10 * sin(arg * i);
     }
 
-
-    
-    std::cout<<"B"<<std::endl;
-       // pthread_mutex_lock(mutex.ptr);
+    std::cout << "B" << std::endl;
+    // pthread_mutex_lock(mutex.ptr);
     memcpy(&vals, str, sizeof(vals));
-    std::cout<<"B"<<std::endl;
-       
-        
+    std::cout << "B" << std::endl;
+
     snd_pcm_hw_params_alloca(&hwparams);
 
     ret = snd_pcm_open(&pcm_handle, pcm_name, stream, 0);
     std::cout << "Opening: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_any(pcm_handle, hwparams);
-    std::cout << "Initializing hwparams structure: " << snd_strerror(ret) << std::endl;   
+    std::cout << "Initializing hwparams structure: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_access(pcm_handle, hwparams,
-            SND_PCM_ACCESS_RW_INTERLEAVED);
+                                       SND_PCM_ACCESS_RW_INTERLEAVED);
     std::cout << "Setting access: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_format(pcm_handle, hwparams,
-            SND_PCM_FORMAT_S16_LE);
+                                       SND_PCM_FORMAT_S16_LE);
     std::cout << "Setting format: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_rate(pcm_handle, hwparams,
-            rate, (int)0);
+                                     rate, (int)0);
     std::cout << "Setting rate: " << snd_strerror(ret) << std::endl;
 
-    ret = snd_pcm_hw_params_set_channels(pcm_handle, hwparams, 2); 
+    ret = snd_pcm_hw_params_set_channels(pcm_handle, hwparams, 2);
     std::cout << "Setting channels: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_periods(pcm_handle, hwparams, 2, 0);
     std::cout << "Setting periods: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params_set_buffer_size_near(pcm_handle, hwparams,
-            &bufferSize);
+                                                 &bufferSize);
     std::cout << "Setting buffer size: " << snd_strerror(ret) << std::endl;
 
     ret = snd_pcm_hw_params(pcm_handle, hwparams);
     std::cout << "Applying parameters: " << snd_strerror(ret) << std::endl;
 
-    long int valsTmp[len+1];
-//pętla jakoś od tąd
+    long int valsTmp[len + 1];
+    //pętla jakoś od tąd
+
+    int loop = 0;
+    while (loop < 30)
+    {
+        pthread_mutex_lock(mutexB.ptr);
         //shared memory receive
-    memcpy(&valsTmp, str, sizeof(valsTmp));
+        memcpy(&valsTmp, str, sizeof(valsTmp));
 
-    clock_t startTime = valsTmp[len];
+        clock_t startTime = valsTmp[len];
 
-    i = 0;
-    for(i; i < len; i = i + 1) {
-        vals[i] = valsTmp[i];
+        i = 0;
+        for (i; i < len; i = i + 1)
+        {
+            vals[i] = valsTmp[i];
         }
 
-    std::chrono::time_point<std::chrono::system_clock> endTimeTmp = std::chrono::system_clock::now();
-    auto duration = endTimeTmp.time_since_epoch();
-    auto endTime = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
-            
-    
+        std::chrono::time_point<std::chrono::system_clock> endTimeTmp = std::chrono::system_clock::now();
+        auto duration = endTimeTmp.time_since_epoch();
+        auto endTime = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
 
-int err;
-    const void* ptra = (const void*)&vals;
-    err = snd_pcm_prepare(pcm_handle);
-    std::cout << "Preparing: " << snd_strerror(err)<< std::endl;
-    while(err!=0)
-    {   
-        err = snd_pcm_prepare(pcm_handle);  
+        int err;
+        const void *ptra = (const void *)&vals;
+        err = snd_pcm_prepare(pcm_handle);
+        std::cout << "Preparing: " << snd_strerror(err) << std::endl;
+        while (err != 0)
+        {
+            err = snd_pcm_prepare(pcm_handle);
+        }
+        snd_pcm_writei(pcm_handle, ptra, len / 4);
+
+        std::printf("loop nr i ;%ld micorseconds; \n", /*a,*/ (endTime - startTime));
+
+        loop++;
+        //pthread_mutex_unlock(mutex.ptr);
+        pthread_mutex_unlock(mutexA.ptr);
     }
-    snd_pcm_writei(pcm_handle, ptra, len/4);
-    
-    std::printf("loop nr i ;%ld micorseconds; \n",/*a,*/(endTime - startTime));
-
 
     shmdt(str);
     shmctl(shmid, IPC_RMID, NULL);
-
-    pthread_mutex_unlock(mutex.ptr);
-    shared_mutex_close(mutex);
-    shared_mutex_destroy(mutex);
+    //shared_mutex_close(mutex);
+    //shared_mutex_destroy(mutex);
 }
 
 void createProc(void (*function)())
@@ -204,18 +210,46 @@ void createProc(void (*function)())
 // compile: g++ mutex.cpp shared_mutex.c -pthread -lstdc++ -pthread -lrt -lm -lasound -o mutex.out
 int main()
 {
+    shared_mutex_t mutexA = shared_mutex_init("/my-mutex");
+    if (mutexA.ptr == NULL)
+    {
+        std::cout << "Could not initialise mutex" << std::endl;
+        return;
+    }
+    shared_mutex_t mutexB = shared_mutex_init("/my-mutex");
+    if (mutexB.ptr == NULL)
+    {
+        std::cout << "Could not initialise mutex" << std::endl;
+        return;
+    }
+
     initSharedMemory();
-    std::cout<<"main"<<std::endl;
-    createProc(processA);
-    std::cout<<"main"<<std::endl;
+    std::cout << "main" << std::endl;
+    //createProc(processA);
+    if (fork() == 0)
+    {
+        processA(mutexA, mutexB);
+        exit(0);
+    }
+    std::cout << "main" << std::endl;
     sleep(3);
-    createProc(processB);
-    std::cout<<"main"<<std::endl;
+    //createProc(processB);
+    if (fork() == 0)
+    {
+        processA(mutexA, mutexB);
+        exit(0);
+    }
+    std::cout << "main" << std::endl;
 
     while (wait(NULL) > 0)
     {
-        
     }
-    
+
+    //shared_mutex_close(mutex);
+    //shared_mutex_destroy(mutex);
+
+    //shared_mutex_close(mutex);
+    //shared_mutex_destroy(mutex);
+
     return 0;
 }
